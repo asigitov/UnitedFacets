8422b2aaae5472631becc4bb9246953e34f3388d
asigitov <github.anton.sigitov@outlook.com>
2016-10-11 14:20:12 +0200

    2displays example and Linux DLLs added

---
+++ Assets/Scripts/UnitedFacets/Input/LeftRightTopDown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftRightTopDown : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(Time.deltaTime * -speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.position += new Vector3(0, Time.deltaTime * -speed, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.position += new Vector3(0, Time.deltaTime * speed, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/UnitedFacets/Manipulation/RotateY.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateY : MonoBehaviour {
+
+    public float speed = 1.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(0, Time.deltaTime * speed, 0);
+	}
+}
